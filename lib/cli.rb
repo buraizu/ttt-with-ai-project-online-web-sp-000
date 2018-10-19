ca00d277acbaa3b92215094b37dd0a4d02a850df
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-19 03:00:19 +0000

    automatically backed up by learn

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,18 +1,23 @@
 class Cli
 
   def play_game
+    loop do
+      puts "Welcome to TicTacToe.  Enter '0' for a simulated game, '1' for a one-player game, and '2' for a two-player game. Enter 'exit' to exit."
+      input = gets.chomp
+        if input == "0"
+          game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"))
+          game.play
+        elsif input == "1"
+          game = Game.new(Players::Computer.new("X"), Players::Human.new("O"))
+          game.play
+        elsif input == "2"
+          game = Game.new(Players::Human.new("X"), Players::Human.new("O"))
+          game.play
+        elsif input.downcase! == 'exit'
+          break
 
-    puts "Welcome to TicTacToe.  Enter '0' for a simulated game, '1' for a one-player game, and '2' for a two-player game. Enter 'exit' to exit."
-    input = gets.chomp
-    if input == "0"
-      game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"))
-      game.play
-    elsif input == "1"
-      game = Game.new(Players::Computer.new("X"), Players::Human.new("O"))
-      game.play
-    elsif input == "2"
-      game = Game.new(Players::Human.new("X"), Players::Human.new("O"))
-      game.play
+        end
+
     end
 
   end
