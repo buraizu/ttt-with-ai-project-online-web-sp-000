06b393007ae2550568054e2ad6d2c8db604ca77c
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-19 02:57:36 +0000

    automatically backed up by learn

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,7 +2,7 @@ class Cli
 
   def play_game
 
-    puts "Welcome to TicTacToe.  Enter '0' for a simulated game, '1' for a one-player game, and '2' for a two-player game."
+    puts "Welcome to TicTacToe.  Enter '0' for a simulated game, '1' for a one-player game, and '2' for a two-player game. Enter 'exit' to exit."
     input = gets.chomp
     if input == "0"
       game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"))
